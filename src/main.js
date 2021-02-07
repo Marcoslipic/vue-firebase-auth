1368c36d8827f4e64b62144b9133066840d5b6e0
Marcoslipic <mjlipic.37@gmail.com>
2021-02-07 15:41:55 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from'firebase'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBHHQczz3BX2DIwOorKqTpcuErKmdJpqaU",
+    authDomain: "asdf-cfb12.firebaseapp.com",
+    projectId: "asdf-cfb12",
+    storageBucket: "asdf-cfb12.appspot.com",
+    messagingSenderId: "647697162287",
+    appId: "1:647697162287:web:e77018367395882d4626da",
+    measurementId: "G-77587CF0S6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
 createApp(App).use(router).mount('#app')
